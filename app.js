0a1423cf09d5207e823e8c21c6327cc3408cc10d
spark156 <lucabergman@hotmail.nl>
2022-01-12 21:18:47 +0100

    save todolist state to localstorage

--- app.js
+++ app.js
@@ -8,11 +8,29 @@ const app = Vue.createApp({
       nextToDoId: 2,
     }
   },
+  beforeMount() {
+    console.log("beforeMount")
+    const storedTodos = JSON.parse(window.localStorage.getItem('todos'))
+    const storedNextToDoId = window.localStorage.getItem('nextToDoId')
+    if ((storedTodos && storedNextToDoId) !== null) {
+      this.todos = storedTodos
+      this.nextToDoId = Number(storedNextToDoId)
+    }
+  },
   methods: {
     addToDo() {
       this.todos.push({ title: this.newToDo, id: this.nextToDoId++ })
       this.newToDo = ''
+      this.setStorage()
     },
+    removeToDo(index) {
+      this.todos.splice(index, 1)
+      this.setStorage()
+    },
+    setStorage() {
+      window.localStorage.setItem('todos', JSON.stringify(this.todos))
+      window.localStorage.setItem('nextToDoId', this.nextToDoId)
+    }
   }
 })
 
